9d6d0374dada46a4bd2b16aaec7c70f686be3130
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-08-28 00:38:46 +0200

    Spdxify.

--- src/uuseg_string.mli
+++ src/uuseg_string.mli
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2014 The uuseg programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 (** Unicode text segmentation on UTF-X OCaml strings.
@@ -83,19 +83,3 @@ val pp_utf_8_lines : Format.formatter -> string -> unit
     (U+000B), LINE SEPARATOR (U+2028), PARAGRAPH SEPARATOR (U+2020),
     NEXT LINE (U+085), LINE FEED (U+000A), CARRIAGE RETURN (U+000D),
     and the sequence CR LF (U+000D, U+000A). *)
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2014 The uuseg programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
